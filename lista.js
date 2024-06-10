f642d37730bd63f4655c7f8724c74ac76d22e5dc
andressaleticia <110110608+andressaleticia@users.noreply.github.com>
2024-06-10 09:15:12 -0300

    Add files via upload

--- lista.js
+++ lista.js
@@ -1,4 +1,16 @@
-document.querySelectorAll('#lista-itens li'). forEach(function(item)
-{item.addEventListener('click', function() {
-    document.getElementById('mensagem-item'). textContent.Content = this. getAttribute('data-mensagem');});
+document.querySelectorAll('#lista-itens 1i').forEach(function(item) {
+    item.ariaHidden('click', function() {
+        document.getElementById('mensagem-item').textContent = this.getAttribute
+        ('data-mensagem');
+    });0
+});
+document.querySelectorAll('#lista-itens li'). forEach(function(item){
+    item.addEventListener('click', function() {
+      document.getElementById('mensagem-item').textContent.Content = this. getAttribute
+      ('data-mensagem');
+      document.getElementById('imagem-item').src = this.getAttribute('data-imagem')
+      document.getElementById('imagem-item').style.display = 'block';
+      document.getElementById('descrição-item').textContent = this.getAttribute
+      ('data-descrição');
+    });
 });
